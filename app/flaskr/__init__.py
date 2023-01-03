56a5870fece2b0371550d72bfb5924c71c1a45db
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2023-01-03 11:01:47 -0500

    api is chosen, jikan

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -1,37 +1,13 @@
 import os
+import requests
 
 from flask import Flask, render_template
 
+app = Flask(__name__)
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+@app.route('/')
+def home():
+    variable = requests.get("https://api.jikan.moe/v4/anime/21/full")
+    return render_template('index.html', variable=variable)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
-        
-    @app.route("/")
-    def home():
-        return render_template('index.html')
-
-    return app
