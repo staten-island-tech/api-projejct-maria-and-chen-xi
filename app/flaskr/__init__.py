978753cac58c892f19780f6e8343e8a17232aca8
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2023-01-04 10:52:14 -0500

    anime page

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -4,10 +4,13 @@ import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-
+ 
 @app.route('/')
 def home():
-    variable = requests.get("https://api.jikan.moe/v4/anime/21/full")
-    return render_template('index.html', variable=variable)
+    return render_template('index.html')
+
+@app.route('/<int:name>')
+def anime(name):
+    variable = requests.get(f"https://api.jikan.moe/v4/anime/{name}/full")
+    return render_template('animePage.html', variable=variable)
 
