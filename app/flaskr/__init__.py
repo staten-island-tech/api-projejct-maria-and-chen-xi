fd0e73a8e119bb346fdfa7fc21fdb079a2bd1b42
Octophobe <mrimerov@gmail.com>
2023-01-09 11:46:35 -0500

    kinda good

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -3,44 +3,45 @@ import requests
 import time
 
 from flask import Flask, render_template
-anime_name = 'Cowboy Bebop'
+# anime_name = 'Cowboy Bebop'
 app = Flask(__name__)
 
-def search(name):
-    count = 0
-    for x in name:
+# def search(name):
+#     count = 0
+#     for x in name:
         
-        if x == '%':
-            Xcount = count
-            for thing in name:
-                if thing == '2':
-                    if Xcount==count+1:
-                        name[count] = ''
-                        name[Xcount] = '+'
-                Xcount = Xcount+1
-        count = count+1
-    print(name)
-    return name
+#         if x == '%':
+#             Xcount = count
+#             for thing in name:
+#                 if thing == '2':
+#                     if Xcount==count+1:
+#                         name[count] = ''
+#                         name[Xcount] = '+'
+#                 Xcount = Xcount+1
+#         count = count+1
+#     print(name)
+#     return name
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    random = requests.get('https://api.jikan.moe/v4/top/anime')
+    return render_template('index.html', top=random)
 
 @app.route('/<name>/')
 def animepage(name):
     # final_name = name.replace("%20", " ")
     # print(final_name)
-    # anime = requests.get(f"https://api.jikan.moe/v4/anime/{final_name}/full")
+    anime = requests.get(f"https://api.jikan.moe/v4/anime/{name}/full")
     # if '200' in str(anime):
     #     return render_template('animePage.html', anime=anime)
     # else:
     #     return render_template('error.html')
-    print(name)
-    final_name = name.replace("%20", "+")
+    # print(name)
+    # final_name = name.replace("%20", "+")
 
-    print(final_name)
-    return(render_template('one piece.html', name=final_name))
+    # print(final_name)
+    return(render_template('animePage.html', anime=anime))
     # if final_name == "One Piece":
     #     return render_template("one piece.html")
     # else:
