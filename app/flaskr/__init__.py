fda56441b263a3e785e719bd2b1ae51a9f82403c
Octophobe <mrimerov@gmail.com>
2023-01-13 11:45:06 -0500

    search functio, trial and error

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -1,41 +1,38 @@
-import os
 import requests
-import time
 
-from flask import Flask, render_template
-# anime_name = 'Cowboy Bebop'
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-# def search(name):
-#     count = 0
-#     for x in name:
-        
-#         if x == '%':
-#             Xcount = count
-#             for thing in name:
-#                 if thing == '2':
-#                     if Xcount==count+1:
-#                         name[count] = ''
-#                         name[Xcount] = '+'
-#                 Xcount = Xcount+1
-#         count = count+1
-#     print(name)
-#     return name
 
-
-@app.route('/')
+@app.route('/', methods =["GET", "POST"])
 def home():
-    top = requests.get('https://api.jikan.moe/v4/top/anime')
-    genres = requests.get('https://api.jikan.moe/v4/genres/anime')
-    return render_template('index.html', top=top, genres=genres)
+    if request.method == "POST":
+       requesting = request.form.get("anime")
+       results = requests.get(f'https://api.jikan.moe/v4/anime?q={requesting}&sfw')
+       print(results.json()['pagination']['items']['count'])
+    if results.json()['pagination']['items']['count'] == 0:
+        return(render_template('noResults.html'))
+    
+    return(render_template('searchPage.html', results=results))
+    # top = requests.get('https://api.jikan.moe/v4/top/anime')
+    # genres = requests.get('https://api.jikan.moe/v4/genres/anime')
+    # return render_template('index.html', top=top, genres=genres)
 
-@app.route('/<name>/')
-def animepage(name):
-    final_name = name.replace("%20", "+")
-    anime = requests.get(f'https://api.jikan.moe/v4/anime?q={final_name}&sfw')
-    print(anime.json())
+@app.route('/search:<search>/')
+def search(search):
+    results = requests.get(f'https://api.jikan.moe/v4/anime?q={search}&sfw')
+    # print(anime.json())
+    print(results.json()['pagination']['items']['count'])
+    if results.json()['pagination']['items']['count'] == 0:
+        return(render_template('noResults.html'))
+    return(render_template('searchPage.html', results=results))
+
+@app.route('/<id>/')
+def animePage(id):
+    anime = requests.get(f'https://api.jikan.moe/v4/anime/{id}/full')
+    # print(anime.json())
     try: 
         if anime.json()['status'] == 404:
             return render_template('error.html', status=anime.json()['status'], message=anime.json()['message'])
     except KeyError:
-        return(render_template('animePage.html', anime=anime))
+        return(render_template('animePage.html', variable=anime))
